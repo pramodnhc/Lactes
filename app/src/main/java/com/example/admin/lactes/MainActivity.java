3dca04b171a1ea4ecf8c38858d97d149f0e7897b
pramodnhc <pummy.2022@gmail.com>
2018-02-24 19:14:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/lactes/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.lactes;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
